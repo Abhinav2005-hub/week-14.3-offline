db099cc4d9a73319cfafabab5f9875b3530bfe9e
Abhinav2005-hub <abhinavsaini995@gmail.com>
2025-09-02 21:41:07 +0530

    data fetching in react

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios';
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState({
+    email: "",
+    name: ""
+  });
+
+
+  useEffect(() => {
+    axios.get("http://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+      .then(res => {
+        setLoading(false);
+        setData(res.data);
+      })
+  }, [])
+
+  if (loading) {
+    return "loading...."
+  }
+
+  return (
+    <div>
+      {data.email}
+      {data.name}
+    </div>
+  )
+}
+
+export default App
